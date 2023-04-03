61633deb0c65b5e5f97bac7e602f6041d0366e93
ayushhh321 <ayush56039@gmail.com>
2023-04-03 13:13:33 +0530

    q2 final

--- Hackerrank_question.cpp
+++ Hackerrank_question.cpp
@@ -1,6 +1,8 @@
 
 //all rights reserved to https://github.com/ayushhh321
 
+//Read 3 numbers from stdin and print their sum to stdout.
+
 // #include <cmath>
 // #include <cstdio>
 // #include <vector>
@@ -9,32 +11,13 @@
 // using namespace std;
 
 
-// int main()
-//  {
-//     int a , b , c ;
-//     cin>>a>>b>>c;
-
-//     cout<<(a+b+c)<<endl;
-
+// int main() {
+//     /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+//     int a;
+//     int b;
+//     int c;
+//     cin >> a >> b >> c;
+//     cout << a + b + c << endl;
 //     return 0;
-
 // }
 
-
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
-    int a;
-    int b;
-    int c;
-    cin >> a >> b >> c;
-    cout << a + b + c << endl;
-    return 0;
-}
