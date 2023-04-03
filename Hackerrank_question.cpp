aeb0751d5c51fb92441244e98a818c69c9cc1eb1
ayushhh321 <ayush56039@gmail.com>
2023-04-03 13:04:51 +0530

    test22

--- Hackerrank_question.cpp
+++ Hackerrank_question.cpp
@@ -1,6 +1,26 @@
 
 //all rights reserved to https://github.com/ayushhh321
 
+// #include <cmath>
+// #include <cstdio>
+// #include <vector>
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+
+
+// int main()
+//  {
+//     int a , b , c ;
+//     cin>>a>>b>>c;
+
+//     cout<<(a+b+c)<<endl;
+
+//     return 0;
+
+// }
+
+
 #include <cmath>
 #include <cstdio>
 #include <vector>
@@ -9,11 +29,12 @@
 using namespace std;
 
 
-int main()
- {
-    int a=1 , b=2 , c=7;
-    
-    
-   cout<<(a+b+c)<<endl; 
-    return 0;    
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a;
+    int b;
+    int c;
+    cin >> a >> b >> c;
+    cout << a + b + c << endl;
+    return 0;
 }
