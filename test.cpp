cd7cf4f8acdcdd9ecb44582282cc54331add5c3e
ayushhh321 <ayush56039@gmail.com>
2023-04-03 10:36:51 +0530

    test1

--- test.cpp
+++ test.cpp
@@ -1,20 +1,9 @@
 #include<iostream>
-
 using namespace std;
 
 int main()
 {
-    
+  cout<<"Hello , World !";
 
-  int ayush[4]={45,33,44,21};
-  
-
-  int i=0;
-  while(i<4);
-  {
-    cout<<"the value of ayush  "<<ayush[i]<<endl;
-    i++;
-
-  }
-    return 0;
-}      
+  return 0;
+}
