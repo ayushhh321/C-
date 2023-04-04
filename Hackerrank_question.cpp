3c0d1321b7409915b41fd76533eafbdf7d3cb07d
ayushhh321 <ayush56039@gmail.com>
2023-04-04 11:08:08 +0530

    rn

--- Hackerrank_question.cpp
+++ Hackerrank_question.cpp
@@ -21,11 +21,11 @@
 //     return 0;
 // }
 
-#include <iostream>
-#include <cstdio>
-using namespace std;
+// #include <iostream>
+// #include <cstdio>
+// using namespace std;
 
-int main() {
+// int main() {
     
-    return 0;
-}
+//     return 0;
+// }
