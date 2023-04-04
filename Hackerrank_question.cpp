2ded8d65f2ec000e6728c01a80150893824f1640
ayushhh321 <ayush56039@gmail.com>
2023-04-04 15:18:49 +0530

    not yet

--- Hackerrank_question.cpp
+++ Hackerrank_question.cpp
@@ -24,21 +24,44 @@
 
 //Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.
 
+// #include <iostream>
+// #include <cstdio>
+// using namespace std;
+
+// int main() {
+//     int a;
+//     cin>>a;
+//     long b;
+//     cin>>b;
+//     char d; 
+//     cin>>d;
+//     float c;
+//     cin>>c;
+//     double e;
+//     cin>>e;
+
+//     return 0;
+// }
+
+
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
 int main() {
+    // Complete the code.
     int a;
-    cin>>a;
     long b;
-    cin>>b;
-    char d; 
-    cin>>d;
-    float c;
-    cin>>c;
-    double e;
-    cin>>e;
-
+    long long c;
+    char d;
+    float e;
+    double f;    
+    scanf("%d %ld %lld %c %f %lf", &a, &b, &c, &d, &e, &f);
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%lld\n", c);
+    printf("%c\n", d);
+    printf("%f\n", e);
+    printf("%lf", f);
     return 0;
 }
