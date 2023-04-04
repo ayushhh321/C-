0d0d46fd7fa1dc864f345b921ea233796294f793
ayushhh321 <ayush56039@gmail.com>
2023-04-04 11:34:37 +0530

    q3

--- Hackerrank_question.cpp
+++ Hackerrank_question.cpp
@@ -20,3 +20,15 @@
 //     cout << a + b + c << endl;
 //     return 0;
 // }
+
+
+//Print each element on a new line in the same order it was received as input. Note that the floating point value should be correct up to 3 decimal places and the double to 9 decimal places.
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
