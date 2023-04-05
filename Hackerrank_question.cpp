78c0ab4852c826947df944c05885b55037c57edc
ayushhh321 <ayush56039@gmail.com>
2023-04-05 22:02:37 +0530

    13

--- Hackerrank_question.cpp
+++ Hackerrank_question.cpp
@@ -44,24 +44,24 @@
 // }
 
 
-// #include <iostream>
-// #include <cstdio>
-// using namespace std;
+#include <iostream>
+#include <cstdio>
+using namespace std;
 
-// int main() {
-//     // Complete the code.
-//     int a;
-//     long b;
-//     long long c;
-//     char d;
-//     float e;
-//     double f;    
-//     scanf("%d %ld %lld %c %f %lf", &a, &b, &c, &d, &e, &f);
-//     printf("%d\n", a);
-//     printf("%ld\n", b);
-//     printf("%lld\n", c);
-//     printf("%c\n", d);
-//     printf("%f\n", e);
-//     printf("%lf", f);
-//     return 0;
-// }
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    long long c;
+    char d;
+    float e;
+    double f;    
+    scanf("%d %ld %lld %c %f %lf", &a, &b, &c, &d, &e, &f);
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%lld\n", c);
+    printf("%c\n", d);
+    printf("%f\n", e);
+    printf("%lf", f);
+    return 0;
+}
