4b5f40a6e8b49723b48e57da56aa2ccb47443e03
ayushhh321 <ayush56039@gmail.com>
2023-09-04 23:18:08 +0530

    .py json

---
+++ json.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Load Excel file into DataFrame
+df = pd.read_excel('demo.xlsx')
+
+# Convert DataFrame to JSON string
+json_data = df.to_json(orient='records')
+
+# Save JSON data to a file
+with open('output.json', 'w') as file:
+    file.write(json_data)
