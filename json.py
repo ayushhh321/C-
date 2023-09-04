10c1429a85c05dc555c5b37b9c43135a8cef2d41
ayushhh321 <ayush56039@gmail.com>
2023-09-04 23:36:16 +0530

    error

--- json.py
+++ json.py
@@ -1,50 +1,12 @@
-# import pandas as pd
+import pandas as pd
 
-# # Load Excel file into DataFrame
-# df = pd.read_excel('C:\Users\HP\Desktop\SHE\C++\demo1.xlsx')
+# Load Excel file into DataFrame
+df = pd.read_excel('C:\Users\HP\Desktop\SHE\C++\demo1.xlsx')
 
-# # Convert DataFrame to JSON string
-# json_data = df.to_json(orient='records')
+# Convert DataFrame to JSON string
+json_data = df.to_json(orient='records')
 
-# # Save JSON data to a file
-# with open('output.json', 'w') as file:
-#     file.write(json_data)
-
-import json
-import openpyxl
-
-def excel_to_json(excel_file, sheet_name):
-  """Converts an Excel file to JSON.
-
-  Args:
-    excel_file: The path to the Excel file.
-    sheet_name: The name of the sheet to convert.
-
-  Returns:
-    A JSON string of the Excel data.
-  """
-
-  wb = openpyxl.load_workbook(excel_file)
-  sheet = wb[sheet_name]
-
-  data = {}
-  for row in range(sheet.max_row):
-    row_data = {}
-    for col in range(sheet.max_column):
-      cell_value = sheet.cell(row, col).value
-      row_data[sheet.cell(1, col).value] = cell_value
-    data[row] = row_data
-
-  json_data = json.dumps(data)
-
-  return json_data
-
-
-if __name__ == "__main__":
-  excel_file = "data.xlsx"
-  sheet_name = "Sheet1"
-
-  json_data = excel_to_json(excel_file, sheet_name)
-
-  print(json_data)
+# Save JSON data to a file
+with open('output.json', 'w') as file:
+    file.write(json_data)
 
