0551649c0ef0dd4f11cf2244752629b3bfe595d1
ayushhh321 <ayush56039@gmail.com>
2024-09-17 14:05:01 +0530

    p1

---
+++ stl.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void print(){
+  cout<<"ayush"<<endl;
+}
+
+int add(int a, int b)
+{
+     return a+b;
+}
+
+int main(){
+print(); //normal call kr diya print ko
+
+int value =add(4,4); // yaha toh upar parameter ki value pass kara diye
+cout<<value; //and print kra diye
+return 0;
+
+}
