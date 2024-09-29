eafdce0ef0331ae12f61c5902373fd4621a83dad
ayushhh321 <ayush56039@gmail.com>
2024-09-29 17:07:28 +0530

    //reverse an string

--- string.cpp
+++ string.cpp
@@ -142,3 +142,18 @@ int main() {
 
     return 0;
 }
+//reverse an string
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin,str);
+    
+    for(int i=str.size()-1;i>=0;--i){
+        cout<<str[i];
+    }
+
+    return 0;
+}
